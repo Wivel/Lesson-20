12309547ab57379f8cedb0bcd01e97b5a7ec0866
Wivel <wiveloriginal@yandex.ru>
2018-10-18 23:20:22 +0300

    4 point

--- mai.js
+++ mai.js
@@ -1,6 +1,13 @@
-
 function go() {
     let elem = document.getElementById('par');
     elem.innerHTML = 'Абзац превратился в h3!';
-    elem.outerHTML = '<h3>'+elem.innerHTML+'</h3>';
+    elem.outerHTML = '<h3>' + elem.innerHTML + '</h3>';
+}
+
+function sumer() {
+    let input1 = +document.getElementById('input1').value;
+    let input2 = +document.getElementById('input2').value;
+    let abz = document.getElementById('sum');
+    let sume = input1 + input2;
+    abz.innerHTML = sume;
 }
