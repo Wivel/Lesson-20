aa3477102282f424874e48c6a42d3f9f060856af
Wivel <wiveloriginal@yandex.ru>
2018-10-18 23:07:11 +0300

    2 point

--- mai.js
+++ mai.js
@@ -1,5 +1,6 @@
 
 function go() {
     let elem = document.getElementById('par');
-    elem.innerHTML = ' Ку-ку, а я <b>ЖИРНЫЙ!</b>'
+    elem.innerHTML = 'Абзац превратился в h3!';
+    elem.outerHTML = '<h3>'+elem.innerHTML+'</h3>';
 }
